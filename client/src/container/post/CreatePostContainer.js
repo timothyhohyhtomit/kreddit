49b3e9b290fe0f3368a0f5c29db9a5535f86fc89
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-07-27 15:00:12 -0700

    modified CreatePostContainer.js so it renders a CreatePostView component

--- client/src/container/post/CreatePostContainer.js
+++ client/src/container/post/CreatePostContainer.js
@@ -1,8 +1,15 @@
 import React from "react";
 
+const TAB_ENUM = {
+    text: 1,
+    images: 2
+};
+
 function CreatePostContainer({ handleCreatePostSubmit }) {
+    // states
+    const [selectedTab, setSelectedTab] = useState(TAB_ENUM.text);
     return (
-        <div></div>
+        <CreatePostView />
     );
 }
 
