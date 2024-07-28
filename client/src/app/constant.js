28fdb0367ea7b796f4a5919fa782009eafea1baa
Timothy Ho <timothyho@news-MacBook-Pro.local>
2024-07-28 15:33:12 -0700

    Worked on handleCreatePostSubmit() handler in CreatePostPage.js so that it checks for invalid title and content

--- client/src/app/constant.js
+++ client/src/app/constant.js
@@ -3,3 +3,28 @@ export const TAB_ENUM = {
     text: 1,
     images: 2
 };
+
+export const post = {
+    create: {
+        tab: {
+            TAB_ENUM: {
+                text: 1,
+                images: 2
+            }
+        },
+        title: {
+            MAX_LENGTH: 300,
+            error: {
+                NOT_EMPTY: "Post title cannot be empty.",
+                EXCEED_LIMIT: "Post title cannot exceed a maximum of 300 characters."
+            }
+        },
+        content: {
+            MAX_LENGTH: 2000,
+            error: {
+                NOT_EMPTY: "Post content cannot be empty.",
+                EXCEED_LIMIT: "Post content cannot exceed a maximum of 2000 characters."
+            }
+        }
+    }
+};
--- client/src/page/post/CreatePostPage.js
+++ client/src/page/post/CreatePostPage.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 import CreatePostView from "../../view/post/CreatePostView";
-
-const TAB_ENUM = {
-    text: 1,
-    images: 2
-};
+import { post } from "../../app/constant";
 
 function CreatePostPage() {
     // tabs
@@ -14,8 +10,32 @@ function CreatePostPage() {
     const [title, setTitle] = useState("");
     // content
     const [content, setContent] = useState("");
+    // error
+    const [error, setError] = useState([]);  // an array of error messages as strings
     // handlers for submit create post request
-    const handleCreatePostSubmit = (e) => {};
+    const handleCreatePostSubmit = (e) => {
+        // check for invalid fields
+        const isValid = true;
+        // if title is empty or exceeds limit, show error message in error section
+        if (title.trim().length === 0) {
+            setError((prev) => [...prev, post.create.title.error.NOT_EMPTY]);
+            isValid = false;
+        } else if (title.trim().length > post.create.title.MAX_LENGTH) {
+            setError((prev) => [...prev, post.create.title.error.EXCEED_LIMIT]);
+            isValid = false;
+        }
+        // if content is empty or exceeds limit, show error message in error section
+        if (content.trim().length === 0) {
+            setError((prev) => [...prev, post.create.content.error.NOT_EMPTY]);
+            isValid = false;
+        } else if (content.trim().length() > post.create.content.MAX_LENGTH) {
+            setError((prev) => [...prev, post.create.content.error.EXCEED_LIMIT]);
+            isValid = false;
+        }
+        if (isValid) {
+            // send a request to server
+        }
+    };
     return (
         <CreatePostView
             selectedTab={selectedTab}
