1ed1f018746c7310fc8f4fbd380ecf1a0f99ef9c
Timothy Ho <timothyho@news-MacBook-Pro.local>
2024-07-27 21:39:00 -0700

    Changed CreatePostSelectTabsView.js so clicking on a specific tab will change the selected tab state.

--- client/src/view/post/CreatePostSelectTabsView.js
+++ client/src/view/post/CreatePostSelectTabsView.js
@@ -3,8 +3,8 @@ import React from "react";
 function CreatePostSelectTabsView({ selectedTab, setSelectedTab }) {
     return (
         <div className="post-create-tabs">
-            <div className="post-create-tab">Text</div>
-            <div className="post-create-tab">Images</div>
+            <div className="post-create-tab" onClick={(e) => setSelectedTab(1)}>Text</div>
+            <div className="post-create-tab" onClick={(e) => setSelectedTab(2)}>Images</div>
         </div>
     );
 }
