0dd0167dbdff480cce70035437029cee0b04eee3
Timothy Ho <timothyho@news-MacBook-Pro.local>
2024-07-27 20:08:08 -0700

    Created CreatePostHeaderView.js; It is a header view for CreatePostPage.js

---
+++ client/src/view/post/CreatePostHeaderView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreatePostHeaderView() {
+    return (
+        <div className="post-create-header">
+            <h1>Create Post</h1>
+        </div>
+    );
+}
+
+export default CreatePostHeaderView;
