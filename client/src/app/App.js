3a0749a5da85939dc34e70ba8dabd90857302632
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-07-29 14:11:36 -0700

    Added router to App.js

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,8 +1,33 @@
 import React, { useCallback, useEffect, useReducer, useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Routes } from "react-router-dom";
+import PostPage from "../page/post/PostPage";
+import CreatePostPage from "../page/post/CreatePostPage";
 
 function App() {
     // STATES
     // user info
     const [isLoggedIn, setLoggedIn] = useState(localStorage.getItem("user_id") !== null);
+    // react router
+    const appRouter = createBrowserRouter(createRoutesFromElements(
+        <Route path="/" element={<></>}>
+            <Route index element={
+                <></>
+            } />
+            <Route path="post">
+                <Route path=":postId" element={
+                    <PostPage
+                        isLoggedIn={false}
+                        user={null}
+                    />
+                } />
+                <Route path="create" element={
+                    <CreatePostPage
+                        isLoggedIn={false}
+                        user={null}
+                    />
+                } />
+            </Route>
+        </Route>
+    ));
+    return <RouterProvider router={appRouter} />
 }
