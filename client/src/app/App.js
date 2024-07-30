80b565280ca386ecd9df174998d155a5bcbbd1eb
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-07-29 20:13:58 -0700

    Add import statement for Link component in PostCommentView.js

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useReducer, useState } from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import PostPage from "../page/post/PostPage";
 import CreatePostPage from "../page/post/CreatePostPage";
 import HomePage from "../page/general/MainPage";
@@ -32,7 +32,7 @@ function App() {
             </Route>
         </Route>
     ));
-    return <RouterProvider router={appRouter} />
+    return <RouterProvider router={appRouter} />;
 }
 
 export default App;
