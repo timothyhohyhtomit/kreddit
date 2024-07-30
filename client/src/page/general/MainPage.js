f1a7bfc518d1c72069cfd2f4a9d9cd969c0061e7
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-07-29 17:46:39 -0700

    Created MainPage.js that is the home page and should be rendered on launching the app

---
+++ client/src/page/general/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import HomeView from "../../view/general/HomeView";
+
+function HomePage({ isLoggedIn, userId }) {
+    return (
+        <HomeView />
+    );
+}
+
+export default HomePage;
---
+++ client/src/view/general/HomeView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeView() {
+    return (
+        <div className="home">
+            <div className="home-banner">Welcome to Kreddit</div>
+        </div>
+    );
+}
+
+export default HomeView;
