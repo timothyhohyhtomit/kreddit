7af8df7a7b5b45e625dccd2c293d010ffbfc5f88
Timothy Ho <timothyho@news-MacBook-Pro.local>
2024-07-30 14:52:26 -0700

    Refined Create Post button in RootHeaderView.js and added a handler for it that navigates to /post/creatte

--- client/src/container/general/RootHeader.js
+++ client/src/container/general/RootHeader.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 import RootHeaderView from "../../view/general/RootHeaderView";
 
 function RootHeader() {
+    // navigate
+    const navigate = useNavigate();
+    // handlers
+    const handleClickCreatePost = (e) => {
+        navigate("/post/create");
+    };
     return (
-        <RootHeaderView />
+        <RootHeaderView
+            handleClickCreatePost={handleClickCreatePost}
+        />
     );
 }
 
--- client/src/view/general/RootHeaderView.js
+++ client/src/view/general/RootHeaderView.js
@@ -2,14 +2,16 @@ import React from "react";
 
 import "./RootHeaderView.css";
 
-function RootHeaderView() {
+function RootHeaderView({ handleClickCreatePost }) {
     return (
         <div className="root-header">
             <div className="root-header-logo">Kreddit</div>
             <div className="root-header-search-bar"></div>
             <div className="root-header-buttons">
-                <div className="root-header-create-post-button">
-                    <span className="root-header-create-post-button-symbol">+</span>
+                <div className="root-header-create-post-button" onClick={handleClickCreatePost}>
+                    <svg rpl="" fill="currentColor" height="20" icon-name="add-outline" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19 9.375h-8.375V1h-1.25v8.375H1v1.25h8.375V19h1.25v-8.375H19v-1.25Z"></path>
+                    </svg>
                     &nbsp;Create
                 </div>
                 <div className="root-header-account">Acc</div>
