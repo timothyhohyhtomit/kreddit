3ee145905b7dee98f70f23ea9d4d217e7c29f2f3
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-07-31 15:24:25 -0700

    Worked on styling of the components in header; Next time implement handlers for search bar such that an empty search result page is rendered on searching

--- client/src/container/general/RootHeader.js
+++ client/src/container/general/RootHeader.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 
 import RootHeaderView from "../../view/general/RootHeaderView";
 
-function RootHeader({ searchQuery, setSearchQuery, handleClickSearch }) {
-    // navigate
+function RootHeader() {
+    // STATES
+    // search bar
+    const [searchQuery, setSearchQuery] = useState("");
+    // NAVIGATE
     const navigate = useNavigate();
-    // handlers
+    // HANDLERS
+    const handleClickSearch = (e) => {};
     const handleClickCreatePost = (e) => {
         navigate("/post/create");
     };
