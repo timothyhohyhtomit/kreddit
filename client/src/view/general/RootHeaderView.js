9e0408ddada6e74eb342895e145ab8be03bff304
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-07-31 14:12:00 -0700

    Created RootHeaderSearchView.js which renders a search bar in the header

--- client/src/view/general/RootHeaderView.js
+++ client/src/view/general/RootHeaderView.js
@@ -9,7 +9,7 @@ function RootHeaderView({ handleClickCreatePost }) {
             <div className="root-header-search-bar"></div>
             <div className="root-header-buttons">
                 <div className="root-header-create-post-button" onClick={handleClickCreatePost}>
-                    <svg rpl="" fill="currentColor" height="20" icon-name="add-outline" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
+                    <svg rpl="" fill="currentColor" height="20" icon-name="plus-sign" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M19 9.375h-8.375V1h-1.25v8.375H1v1.25h8.375V19h1.25v-8.375H19v-1.25Z"></path>
                     </svg>
                     &nbsp;Create
