0aa993a2c5b497a01a19df9fed93d2c2a284ac68
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-08-03 12:13:47 -0700

    Created SearchResultOptionsView.js and SearchResultOptionsView.css; They are currently empty

---
+++ client/src/view/general/search-result/SearchResultOptionsView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./SearchResultOptionsView.css";
+
+function SearchResultOptionsView() {
+    return (
+        <div className="search-result-options">options</div>
+    );
+}
+
+export default SearchResultOptionsView;
