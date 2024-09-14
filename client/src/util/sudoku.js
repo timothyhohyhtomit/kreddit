60fbf97199c6c7f483556b14637c120ef4a4812a
Timothy Ho <timothyhohyhtomit@gmail.com>
2024-09-14 14:00:26 -0700

    Created sudoku.js in the front-end, used to implement an algorithm to solve sudoku puzzles. Next time implement the solveSudoku() method, along with any necessary helper methods

---
+++ client/src/util/sudoku.js
@@ -0,0 +1,11 @@
+/* sudoku.js is a module that consists of relevant methods for sudoku implementation.
+*/
+
+/*
+solveSudoku solves the supplied problem using backtracking and returns the solved puzzle as a string of length 81.
+Arguments:
+- problem: string of length 81, consisting of digits (1-9 for pre-filled cells and 0 for empty cells)
+Returns:
+- a string of length 81, consisting of digits (1-9)
+*/
+export const solveSudoku = (problem) => {};
